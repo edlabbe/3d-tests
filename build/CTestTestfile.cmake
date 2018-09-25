ec6444b13717ffed6f8993bee248052ed39baf26
edlabbe <edlabbe@gmail.com>
2018-09-25 13:56:51 -0400

    vsc pcl with includes

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ed/Projects/git-workspace/3d-tests
-# Build directory: /home/ed/Projects/git-workspace/3d-tests/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,43 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/common/io.h>
 
-int main(int, char**) 
+#include "factory/edge_weight_computer_factory.h"
+#include "factory/graph_builder_factory.h"
+
+#include "random_walker_segmentation.h"
+
+typedef pcl::PointXYZRGB PointT;
+typedef pcl::PointXYZRGBNormal PointWithNormalT;
+typedef pcl::Normal NormalT;
+
+typedef pcl::PointCloud<PointT> PointCloudT;
+typedef pcl::PointCloud<PointWithNormalT> PointWithNormalCloudT;
+typedef pcl::PointCloud<NormalT> NormalCloudT;
+
+typedef typename pcl::segmentation::RandomWalkerSegmentation<pcl::PointXYZRGB>::Graph Graph;
+typedef typename pcl::segmentation::RandomWalkerSegmentation<pcl::PointXYZRGB>::GraphPtr GraphPtr;
+
+int main(int argc, char** argv) 
 {
-    std::cout << "Hello, world 2!\n";
+    std::cout << "Program Start\n";
+
+    factory::EdgeWeightComputerFactory<Graph> wc_factory;
+    factory::GraphBuilderFactory<PointWithNormalT, Graph> gb_factory;
+
+    if (argc < 2 || pcl::console::find_switch (argc, argv, "--help"))
+    {
+        pcl::console::print_error ("Usage: %s <pcd-file>\n"
+                                "--save-seeds <pcd-file>\n"
+                                "--load-seeds <pcd-file>\n"
+                                "--save\n"
+                                "--save-clusters\n"
+                                "--potential\n"
+                                "--fixed-colors\n"
+                                "%s\n"
+                                "%s\n"
+                                , argv[0]
+                                , wc_factory.getUsage ().c_str ()
+                                , gb_factory.getUsage ().c_str ());
+        return (1);
+    }
 }
